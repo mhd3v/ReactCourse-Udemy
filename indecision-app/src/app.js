a42e09e65b448025df812bc4f9d6744569d1b0b1
mhd3v <mamir3012@gmail.com>
2019-07-12 23:59:21 +0500

    Configure babel to compile JSX to es5 JS

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,6 @@
-'use strict';
-
 // JSX - JavaScript XML -> JavaScript extension, just like SCSS is for CSS
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 // ReactDOM has pretty much one method that we'll use
